ef2f9f8245791db861202f3d9142a807be4514d2
ManojKumarBV <58429776+ManojKumarBV@users.noreply.github.com>
2023-03-01 15:17:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Oblo</h1>
-          <h1 className="Sub-title">we build smart solution for your homes</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+           <h1 className="Sub-title">we build smart solution for your homes</h1>
         </p>
       </div>
     );
